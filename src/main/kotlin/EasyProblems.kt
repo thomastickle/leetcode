08db1eca5767aff90a6dc50488caaa1d27600f9b
Thomas William Tickle <ttickle@gmail.com>
2023-10-29 22:23:35 -0400

    Completed problem 21.

--- src/main/kotlin/EasyProblems.kt
+++ src/main/kotlin/EasyProblems.kt
@@ -2,7 +2,7 @@ import java.util.*
 
 // LeetCode Problem 1
 fun twoSum(nums: IntArray, target: Int): IntArray {
-    for (i in 0 until nums.size) {
+    for (i in nums.indices) {
         val remainder = target - nums[i]
         for (j in (i + 1)..<nums.size) {
             if (nums[j] == remainder) {
@@ -21,12 +21,12 @@ fun isPalindrome(x: Int): Boolean {
     var end = asString.length - 1
     while (start <= end) {
         if (asString[start] != asString[end]) {
-            return false;
+            return false
         }
         start += 1
         end -= 1
     }
-    return true;
+    return true
 }
 
 // LeetCode Problem 13
@@ -35,8 +35,8 @@ val ROMAN_NUMERAL_LOOKUP_MAP =
     mapOf(Pair('I', 1), Pair('V', 5), Pair('X', 10), Pair('L', 50), Pair('C', 100), Pair('D', 500), Pair('M', 1000))
 
 fun romanToInt(s: String): Int {
-    var accumulator = 0;
-    var i = 0;
+    var accumulator = 0
+    var i = 0
     while (i < s.length) {
         val x = ROMAN_NUMERAL_LOOKUP_MAP[s[i]]!!
         if (i < (s.length - 1) && x < ROMAN_NUMERAL_LOOKUP_MAP[s[i + 1]]!!) {
@@ -49,7 +49,7 @@ fun romanToInt(s: String): Int {
         }
     }
 
-    return accumulator;
+    return accumulator
 }
 
 // Problem 14
@@ -62,7 +62,7 @@ fun longestCommonPrefix(strs: Array<String>): String {
         if (commonString.length > value.length) {
             commonString = commonString.substring(0, value.length)
         }
-        for (i in 0..<commonString.length) {
+        for (i in commonString.indices) {
             if (commonString[i] != value[i]) {
                 commonString = commonString.substring(0, i)
                 break
@@ -85,7 +85,7 @@ fun isPair(firstChar: Char, secondChar: Char): Boolean {
 fun isValid(s: String): Boolean {
     val stack = Stack<Char>()
 
-    for (i in 0..< s.length) {
+    for (i in s.indices) {
         when(s[i]) {
             '(','{', '[' -> stack.push(s[i])
             else -> {
@@ -95,9 +95,55 @@ fun isValid(s: String): Boolean {
                 if (!isPair(stack.peek(), s[i])) {
                     return false
                 }
-                stack.pop();
+                stack.pop()
             }
         }
     }
     return stack.isEmpty()
+}
+
+// Problem 21: Merge Two Sorted Lists
+class  ListNode(var `val`: Int) {
+    var next: ListNode? = null
+}
+
+fun constructListFromValues(vararg values: Int): ListNode? {
+    var head: ListNode? = null
+    var current: ListNode? = null
+    values.forEach {
+        if (head == null) {
+            head = ListNode(it)
+            current = head
+        } else {
+            current?.next = ListNode(it)
+            current = current?.next
+        }
+    }
+    return head
+}
+
+fun mergeTwoLists(list1: ListNode?, list2: ListNode?) : ListNode? {
+    val result = ListNode(0)
+    var head1 = list1
+    var head2 = list2
+    var current: ListNode? = result
+
+    while(head1 != null && head2 != null) {
+        if (head1.`val` < head2.`val`) {
+            current?.next = head1
+            head1 = head1.next
+        } else {
+            current?.next = head2
+            head2 = head2.next
+        }
+        current = current?.next
+    }
+
+    if (head1 != null) {
+        current?.next = head1
+    } else {
+        current?.next = head2
+    }
+
+    return result.next
 }
--- src/test/kotlin/EasyProblemsKtTest.kt
+++ src/test/kotlin/EasyProblemsKtTest.kt
@@ -6,7 +6,7 @@ class EasyProblemsKtTest {
 
     @Test
     fun twoSum() {
-        val nums: IntArray = intArrayOf(2,7, 11, 15);
+        val nums: IntArray = intArrayOf(2,7, 11, 15)
         val target = 9
         val indexes = twoSum(nums, target)
         assertArrayEquals(intArrayOf(0,1), indexes)
@@ -54,4 +54,42 @@ class EasyProblemsKtTest {
         assertFalse(isValid("["))
         assertFalse(isValid("]"))
     }
+
+    @Test
+    fun constructListFromArray() {
+        val list = constructListFromValues(1, 2, 4)
+        assertNotNull(list)
+        assertEquals(1, list!!.`val`)
+        assertEquals(2, list.next!!.`val`)
+        assertEquals(4, list.next!!.next!!.`val`)
+
+        val list2 = constructListFromValues()
+        assertNull(list2)
+    }
+
+    private fun verifyList(values: List<Int>, list: ListNode?): Boolean {
+        var current = list;
+        for ( i in values) {
+            if (current == null || i != current.`val`) {
+                return false
+            }
+            current = current.next
+        }
+        return true
+    }
+
+    @Test
+    fun mergeTwoLists() {
+        val list = mergeTwoLists(constructListFromValues(1, 2, 4), constructListFromValues(1, 3, 4))
+        assertTrue(verifyList(listOf(1, 1, 2, 3, 4, 4), list))
+
+        val list2 = mergeTwoLists(null, null)
+        assertTrue(verifyList(emptyList(), list2))
+
+        val list3 = mergeTwoLists(null, constructListFromValues(0))
+        assertTrue(verifyList(emptyList(), list3))
+    }
+
+
+
 }
