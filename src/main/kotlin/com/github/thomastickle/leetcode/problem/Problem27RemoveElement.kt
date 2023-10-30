e00d447ae7022646ccedf837f280b8836dc7fcde
Thomas William Tickle <ttickle@gmail.com>
2023-10-30 00:08:10 -0400

    Made it faster, but memory is still tricky.

--- src/main/kotlin/com/github/thomastickle/leetcode/problem/Problem27RemoveElement.kt
+++ src/main/kotlin/com/github/thomastickle/leetcode/problem/Problem27RemoveElement.kt
@@ -2,10 +2,11 @@ package com.github.thomastickle.leetcode.problem
 
 class Problem27RemoveElement {
     fun removeElement(nums: IntArray, `val`: Int): Int {
-        val toList = nums.filter { it != `val` }.toIntArray()
-        toList.forEachIndexed{ index, element ->
-            nums[index] = element
+        var count = 0
+        nums.forEach {
+            if (it != `val`)
+                nums[count++] = it
         }
-        return toList.size
+        return count
     }
 }
