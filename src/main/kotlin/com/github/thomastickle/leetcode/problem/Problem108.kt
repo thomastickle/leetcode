7a886dc9bb3a4c262b562e3d53542b0c36e87184
Thomas William Tickle <ttickle@gmail.com>
2023-11-01 18:22:41 -0400

    Completed problem 108 of constructing a balanced tree.

---
+++ src/main/kotlin/com/github/thomastickle/leetcode/problem/Problem108.kt
@@ -0,0 +1,18 @@
+package com.github.thomastickle.leetcode.problem
+
+class Problem108 {
+    fun sortedArrayToBST(nums: IntArray): TreeNode? {
+        fun insertNodeIntoTree(nums: IntArray, left: Int, right: Int): TreeNode? {
+            if (left > right) return null
+            val mid = left + (right - left) / 2
+            val node = TreeNode(nums[mid])
+            node.left = insertNodeIntoTree(nums, left, mid - 1)
+            node.right = insertNodeIntoTree(nums, mid + 1, right)
+            return node;
+        }
+
+
+        if (nums.isEmpty()) return null
+        return insertNodeIntoTree(nums, 0, nums.size - 1)
+    }
+}
